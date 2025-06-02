c9934930ed58615610fd00958195136a67ff1a03
bneuhausz <bneuhausz@gmail.com>
2025-06-02 11:18:21 +0200

    added docker-compose for demo purposes

--- AuthService/AuthService/Program.cs
+++ AuthService/AuthService/Program.cs
@@ -27,8 +27,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
--- UserManagerApi/UserManagerApi/Program.cs
+++ UserManagerApi/UserManagerApi/Program.cs
@@ -46,8 +46,6 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthentication();
 
 app.UseMiddleware<UserContextMiddleware>();
