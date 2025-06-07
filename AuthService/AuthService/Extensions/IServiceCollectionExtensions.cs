c7979846e2b6ea8144297f8a4d757e5616772ec6
bneuhausz <bneuhausz@gmail.com>
2025-06-07 17:12:49 +0200

    cleaned up program.cs in authservice

---
+++ AuthService/AuthService/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AuthService.BackgroundServices;
+using AuthService.Config;
+using AuthService.Data;
+using AuthService.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<AuthDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("AuthDb")));
+
+    public static void ConfigureOptions(this IServiceCollection services) =>
+        services.AddOptions<JwtOptions>().BindConfiguration(JwtOptions.Section);
+
+    public static void ConfigureSingletonServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IPasswordHasher<object>, PasswordHasher<object>>();
+        services.AddSingleton<PasswordVerificationService>();
+    }
+    
+    public static void ConfigureHostedServices(this IServiceCollection services) =>
+        services.AddHostedService<RefreshTokenCleanupService>();
+}
--- AuthService/AuthService/Program.cs
+++ AuthService/AuthService/Program.cs
@@ -1,37 +1,20 @@
-using AuthService.BackgroundServices;
-using AuthService.Config;
-using AuthService.Data;
-using AuthService.Services;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Serilog;
+using AuthService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .CreateLogger();
+builder.Host.ConfigureLogging(builder.Configuration);
 
-builder.Host.UseSerilog();
-
-// Add services to the container.
-builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthDb")));
-
-builder.Services.AddOptions<JwtOptions>().BindConfiguration(JwtOptions.Section);
-
-builder.Services.AddSingleton<IPasswordHasher<object>, PasswordHasher<object>>();
-builder.Services.AddSingleton<PasswordVerificationService>();
-
-builder.Services.AddHostedService<RefreshTokenCleanupService>();
+builder.Services.ConfigureDatabase(builder.Configuration);
+builder.Services.ConfigureOptions();
+builder.Services.ConfigureSingletonServices();
+builder.Services.ConfigureHostedServices();
 
 builder.Services.AddControllers();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
